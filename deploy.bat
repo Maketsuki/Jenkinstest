9ad82735f184d843338856e212f632808eb6817c
Markus Sorjonen <markus.sorjonen@gmail.com>
2023-08-30 17:47:29 +0300

    deploy was wrong

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,14 @@
 cd server
-npm install
-
+if %errorlevel% neq 0 exit /b %errorlevel%
+call npm install
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../client
-npm install
-npm run build
-
+if %errorlevel% neq 0 exit /b %errorlevel%
+call npm install
+if %errorlevel% neq 0 exit /b %errorlevel%
+call npm run build
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../server
-pm2 restart index.js
+if %errorlevel% neq 0 exit /b %errorlevel%
+call pm2 restart index.js
+if %errorlevel% neq 0 exit /b %errorlevel%
